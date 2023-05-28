5071626c59879f4aff749510921056189597324f
gojalifs <fajarsidik1999@gmail.com>
2023-05-28 19:07:05 +0700

    changed statement

--- database/migrations/2023_05_09_235147_create_get_presensi_procedure.php
+++ database/migrations/2023_05_09_235147_create_get_presensi_procedure.php
@@ -15,7 +15,7 @@ return new class extends Migration {
         DB::unprepared('
             CREATE DEFINER=`rudi`@`localhost` PROCEDURE `get_presensi`(IN tanggal DATE)
             BEGIN
-                SELECT u.nama, u.nik, u.nipns, p.id_presensi, p.tanggal, pd.jenis, pd.jam, pd.longitude, pd.latitude, pd.img_path
+                SELECT ROW_NUMBER() OVER () AS id, u.nama, u.nik, u.nipns, p.id_presensi, p.tanggal, pd.jenis, pd.jam, pd.longitude, pd.latitude, pd.img_path
                 FROM users u
                 LEFT JOIN presensis p ON u.nik = p.nik AND p.tanggal = tanggal
                 LEFT JOIN presensi_details pd ON p.id_presensi = pd.id_presensi
