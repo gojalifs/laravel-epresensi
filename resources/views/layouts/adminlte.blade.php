b42044a03f3cdd85e16a54948759cf3ee1d3667b
gojalifs <fajarsidik1999@gmail.com>
2023-05-28 13:12:00 +0700

    changed on school name

--- resources/views/layouts/adminlte.blade.php
+++ resources/views/layouts/adminlte.blade.php
@@ -54,7 +54,7 @@
                 <img src="{{ asset('vendor/adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
 
-                <span class="brand-text font-weight-light">SMPN 1 <br>Karangbahagia</span>
+                <span class="brand-text font-weight-light">SMPN 1 <br>Karang Bahagia</span>
             </a>
 
             <!-- Sidebar -->
