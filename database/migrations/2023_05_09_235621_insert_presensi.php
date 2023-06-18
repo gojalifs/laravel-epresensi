7d5888337a43927f8a25ff49874600a98ba0583a
gojali99 <fajarsidik1999@gmail.com>
2023-06-18 20:11:48 +0700

    fixed procedure insert

--- database/migrations/2023_05_09_235621_insert_presensi.php
+++ database/migrations/2023_05_09_235621_insert_presensi.php
@@ -22,7 +22,8 @@ return new class extends Migration {
         )
         BEGIN
             DECLARE presensi_id varchar(10);
-            DECLARE count int;
+            DECLARE count_user int;
+            DECLARE count_today int;
             DECLARE p_tanggal DATE;
             DECLARE p_jam TIME;
         
@@ -31,18 +32,23 @@ return new class extends Migration {
             -- get id presensi
             SET presensi_id = (SELECT id_presensi FROM presensis WHERE tanggal = DATE(NOW()) AND nik = p_nik);
             SET p_tanggal = date(NOW());
-            SET P_jam = time(now());
+            SET p_jam = time(now());
             
             -- check today data
-            SELECT COUNT(*) INTO count FROM presensis WHERE tanggal = p_tanggal and nik = p_nik;
-            IF(count = 0) THEN 
-                SET presensi_id = CONCAT(DATE_FORMAT(NOW(), '%d%m%y'), LPAD((SELECT COUNT(*) + 1 FROM presensis), 4, '0'));
+            SELECT COUNT(*) INTO count_user FROM presensis WHERE tanggal = p_tanggal and nik = p_nik;
+            SELECT COUNT(*) INTO count_today FROM presensis WHERE tanggal = p_tanggal;
+            IF(count_user = 0) THEN 
+            	IF (count_today = 0) THEN
+                	SET presensi_id = CONCAT(DATE_FORMAT(NOW(), '%d%m%y'), LPAD('1', 4, '0'));
+                ELSE
+                	SET presensi_id = CONCAT(DATE_FORMAT(NOW(), '%d%m%y'), LPAD((SELECT COUNT(*) + 1 FROM presensis WHERE tanggal = p_tanggal), 4, '0'));
+                END IF;
                 INSERT INTO presensis (id_presensi, nik, tanggal) VALUES (presensi_id, p_nik, p_tanggal);
             END IF;
         
             -- check for details if already
-            select COUNT(*) INTO count FROM presensi_details WHERE id_presensi = presensi_id COLLATE utf8mb4_general_ci AND jenis = p_jenis COLLATE utf8mb4_general_ci;
-            IF(count = 0) THEN 
+            select COUNT(*) INTO count_user FROM presensi_details WHERE id_presensi = presensi_id COLLATE utf8mb4_general_ci AND jenis = p_jenis COLLATE utf8mb4_general_ci;
+            IF(count_user = 0) THEN 
                 INSERT INTO presensi_details (id_presensi, jenis, jam, longitude, latitude, img_path)
                 VALUES (presensi_id, p_jenis, p_jam, p_longitude, p_latitude, p_img_path);
             ELSE
